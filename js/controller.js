b62604c2c96c15e35136318c11587524a193aa39
Tokoy <zhangke5758@126.com>
2016-12-29 14:33:47 +0800

    Add files via upload

--- js/controller.js
+++ js/controller.js
@@ -1,6 +1,6 @@
 'use strict';
 // configure our routes
-app.config(function($routeProvider) {
+app.config(function($routeProvider,$locationProvider) {
 	$routeProvider
 
 		// route for the home page
@@ -20,15 +20,29 @@ app.config(function($routeProvider) {
 			templateUrl : 'pages/register.html',
 			controller  : ''
 		})
-
+		 // use the HTML5 History API
+        $locationProvider.html5Mode(true);
 });
 // create the controller and inject Angular's $scope
-app.controller('myCtrl', function($scope) {
+app.controller('myCtrl', function($scope,$http) {
 	$scope.visible = false;
 	$scope.toggle = function(){
 		$scope.visible = !$scope.visible;
 	}
-	});
-
 	
+	$http.get("./test.json").success(function(response){
+		$scope.datas = response;
+		//alert(JSON.stringify($scope.datas));//这样获取到的只是整个json的内容。
+		//alert(JSON.stringify($scope.datas.id));这样返回的值是undefined。
+	}).error(function(){})
+
+	});
+$(function(){
+        $(".jump").click(function(){
+            $("#mymodal").modal("toggle");
+        });
+		$('.carousel').carousel({
+		  interval: 8000
+		})
+    });
 
